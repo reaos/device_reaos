95941e43b54712fcbddf561965ccef8ba8368505
Maxim Lapunin <majagyage@gmail.com>
2025-04-12 13:54:13 +0200

    Build gapps if it synced

--- reaos_arm64_only/device.mk
+++ reaos_arm64_only/device.mk
@@ -2,3 +2,7 @@ PRODUCT_COPY_FILES += \
     device/reaos/mediacodec.policy.arm:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
 
 $(call inherit-product, device/reaos-prebuilts/prebuilts_arm.mk)
+
+ifneq ("$(wildcard vendor/gapps/arm64)", "")
+    $(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)
+endif
